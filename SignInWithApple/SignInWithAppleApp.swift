cc8440541bae0366fe439e5fdc556281208891a1
Heiko von Wegerer <heiko.von-wegerer@deutschebahn.com>
2021-09-29 19:33:22 +0200

    Initial project commit

---
+++ SignInWithApple/SignInWithAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignInWithAppleApp.swift
+//  SignInWithApple
+//
+//  Created by Heiko von Wegerer on 29.09.21.
+//
+
+import SwiftUI
+
+@main
+struct SignInWithAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
